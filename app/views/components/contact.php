7ab3e5a3243dfd02f410849b63a1e615d2433e1f
sayedsahin <sahin.sayed1@gmail.com>
2022-01-22 06:16:45 +0600

    mail html

---
+++ app/views/components/contact.php
@@ -0,0 +1,35 @@
+<div class="row justify-content-center">
+    <div class="col-lg-8 col-xl-7">
+        <form id="contactForm" method="post" action="<?= BASE_URL; ?>/contact/store">
+            <!-- Name input-->
+            <div class="form-floating mb-3">
+                <input name="name" class="form-control" id="name" type="text" placeholder="Enter your name..." required />
+                <label for="name">Full name*</label>
+            </div>
+            <!-- Email address input-->
+            <div class="form-floating mb-3">
+                <input name="email" class="form-control" id="email" type="email" placeholder="name@example.com" required />
+                <label for="email">Email address*</label>
+            </div>
+            <!-- Phone number input-->
+            <div class="form-floating mb-3">
+                <input name="phone" class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" />
+                <label for="phone">Phone number (optional)</label>
+            </div>
+            <!-- Message input-->
+            <div class="form-floating mb-3">
+                <textarea name="message" class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem"></textarea>
+                <label for="message">Message*</label>
+            </div>
+
+            <div id="submitMessage">
+                <?php
+                    helper(['message']);
+                    message();
+                ?>
+            </div>
+            <!-- Submit Button-->
+            <button class="btn btn-primary btn-xl" id="submitButton" type="submit">Send</button>
+        </form>
+    </div>
+</div>
