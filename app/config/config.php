b043cb8d2965af7337c0a63e907d403d9c5d2c80
sayedsahin <sahin.sayed1@gmail.com>
2022-01-24 01:56:20 +0600

    config rechange

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,9 @@
 <?php
-	/*define('BASE_URL', 'http://portfolio.test');
+	define('BASE_URL', 'http://portfolio.test');
 	define("DB_HOST", "localhost");
 	define("DB_TYPE", "mysql");
 	define("DB_PORT", 3306);
 	define("DB_USER", "root");
 	define("DB_PASS", "root");
-	define("DB_NAME", "portfolio");*/
-
-	define('BASE_URL', 'https://sayedsahin.herokuapp.com');
-	define("DB_HOST", "ec2-3-216-113-109.compute-1.amazonaws.com");
-	define("DB_TYPE", "mysql");
-	define("DB_PORT", 5432);
-	define("DB_USER", "fgulakqybsyrws");
-	define("DB_PASS", "e3faf06d00e798fd0199242af48263799ef1f41c154f76c6740d9a274822d24f");
-	define("DB_NAME", "dbdeppgreps801");
+	define("DB_NAME", "portfolio");
  ?>
