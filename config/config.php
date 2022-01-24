aee6561a24f8fbb49e79f1c403beb762aa5d89e3
sayedsahin <sahin.sayed1@gmail.com>
2022-01-25 05:54:09 +0600

    symphony dotenv file create and configure

--- config/config.php
+++ config/config.php
@@ -1,9 +1,12 @@
 <?php
-	define('BASE_URL', 'http://portfolio.test');
-	define("DB_HOST", "localhost");
-	define("DB_TYPE", "mysql");
-	define("DB_PORT", 3306);
-	define("DB_USER", "root");
-	define("DB_PASS", "root");
-	define("DB_NAME", "portfolio");
+
+	define('BASE_URL', $_ENV['BASE_URL']);
+	
+	define("DB_CONNECTION", $_ENV['DB_CONNECTION']);
+	define("DB_HOST", $_ENV['DB_HOST']);
+	define("DB_PORT", $_ENV['DB_PORT']);
+	define("DB_NAME", $_ENV['DB_NAME']);
+	define("DB_USERNAME", $_ENV['DB_USERNAME']);
+	define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
+	
  ?>
