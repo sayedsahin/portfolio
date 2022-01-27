9409103680a35a915ae475cff6bb6b6958adb4f8
sayedsahin <sahin.sayed1@gmail.com>
2022-01-28 01:46:41 +0600

    .env load

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
-	define('BASE_URL', $_SERVER['BASE_URL']);
+
+	define('BASE_URL', $_ENV['BASE_URL']);
 	define('DEBUG_MODE', true);
-	define("DB_CONNECTION", $_SERVER['DB_CONNECTION']);
-	define("DB_HOST", $_SERVER['DB_HOST']);
-	define("DB_PORT", $_SERVER['DB_PORT']);
-	define("DB_NAME", $_SERVER['DB_NAME']);
-	define("DB_USERNAME", $_SERVER['DB_USERNAME']);
-	define("DB_PASSWORD", $_SERVER['DB_PASSWORD']);
+	define("DB_CONNECTION", $_ENV['DB_CONNECTION']);
+	define("DB_HOST", $_ENV['DB_HOST']);
+	define("DB_PORT", $_ENV['DB_PORT']);
+	define("DB_NAME", $_ENV['DB_NAME']);
+	define("DB_USERNAME", $_ENV['DB_USERNAME']);
+	define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
 	
  ?>
