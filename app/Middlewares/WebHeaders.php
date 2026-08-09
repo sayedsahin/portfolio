686887790ed2d0627fc440ed0db86b0b906fec5e
sayedsahin <sahinsayed@gmail.com>
2026-08-09 18:37:36 +0600

    change webHeader

--- app/Middlewares/WebHeaders.php
+++ app/Middlewares/WebHeaders.php
@@ -14,13 +14,17 @@ class WebHeaders implements MiddlewareInterface
         header('Referrer-Policy: strict-origin-when-cross-origin');
 
         header(
-            "Content-Security-Policy: " .
-                "default-src 'self'; " .
-                "script-src 'self' https://cdn.jsdelivr.net; " .
-                "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " .
-                "img-src 'self' data:; " .
-                "connect-src 'self' https://cdn.jsdelivr.net;"
-        );
+        "Content-Security-Policy: " .
+            "default-src 'self'; " .
+            // 'unsafe-inline' যোগ করা হয়েছে যাতে Inline Script চলতে পারে
+            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; " .
+            // Google Fonts CSS এবং আপনার লোকাল সাইটের জন্য অনুমতি যোগ করা হয়েছে
+            "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://fonts.googleapis.com http://portfolio-new.test; " .
+            // Google Fonts-এর ফন্ট ফাইলগুলোর অনুমতি দেওয়া হয়েছে
+            "font-src 'self' https://fonts.gstatic.com data:; " .
+            "img-src 'self' data:; " .
+            "connect-src 'self' https://cdn.jsdelivr.net;"
+    );
 
         return null;
     }
